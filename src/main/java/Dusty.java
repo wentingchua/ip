b7cf4f261d40e85ebd564ef9c67967f1858a1faa
wentingchua <wentingchua@gmail.com>
2025-01-21 23:36:17 +0800

    Change greeting statement and added exit statement

--- src/main/java/Dusty.java
+++ src/main/java/Dusty.java
@@ -5,6 +5,6 @@ public class Dusty {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello! I'm Dusty\nHow can I help you?\n_________________\nBye! See you next time!\n");
     }
 }

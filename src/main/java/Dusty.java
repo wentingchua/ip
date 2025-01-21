5c3ebddf1fef4e9a5468cd93d6c106b328623754
wentingchua <wentingchua@gmail.com>
2025-01-22 00:15:52 +0800

    Enabled Dusty to show tasks with empty checkboxes

--- src/main/java/Dusty.java
+++ src/main/java/Dusty.java
@@ -1,3 +1,4 @@
+import java.sql.Array;
 import java.util.ArrayList;
 import java.util.Scanner;
 public class Dusty {
@@ -8,6 +9,7 @@ public class Dusty {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         ArrayList<String> store = new ArrayList<String>();
+        ArrayList<String> check = new ArrayList<String>();
         Scanner scan = new Scanner(System.in); // create scanner object
         System.out.println("Hello! I'm Dusty\nHow can I help you?\n_______________\n");
         while (true) {
@@ -16,11 +18,14 @@ public class Dusty {
                 System.out.println("    " + "Bye! See you next time!\n");
                 break;
             } else if (input.equals("list")) {
+                System.out.println("    Here are the tasks in your list:");
                 for (int i = 1; i < store.size() + 1; i++) {
-                    System.out.println("    " + i + ". " + store.get(i - 1));
+                    System.out.println("    " + i + "." + check.get(i - 1) + store.get(i - 1));
                 }
             } else {
                 store.add(input);
+                String checkbox = "[ ] ";
+                check.add(checkbox);
                 System.out.println("    added: "+ input);
             }
         }

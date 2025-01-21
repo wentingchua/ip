e2994e47477868b3cde2dbdbfe7832685da0aceb
wentingchua <wentingchua@gmail.com>
2025-01-21 23:35:26 +0800

    Renamed 'Duke' to 'Dusty'

--- src/main/java/Dusty.java
+++ src/main/java/Dusty.java
@@ -1,4 +1,4 @@
-public class Duke {
+public class Dusty {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"

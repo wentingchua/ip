b4e2601ffa57f8d7a973fa91ace6926afb764ea9
wentingchua <wentingchua@gmail.com>
2025-01-22 00:01:12 +0800

    Enabled Dusty to store text entered by user

--- src/main/java/Dusty.java
+++ src/main/java/Dusty.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 public class Dusty {
     public static void main(String[] args) {
@@ -6,15 +7,17 @@ public class Dusty {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
+        ArrayList<String> store = new ArrayList<String>();
         Scanner scan = new Scanner(System.in); // create scanner object
         System.out.println("Hello! I'm Dusty\nHow can I help you?\n_______________\n");
         while (true) {
             String input = scan.nextLine(); // read user input
             if (input.equals("bye")) {
-                System.out.println("    "+"Bye! See you next time!\n");
+                System.out.println("    " + "Bye! See you next time!\n");
                 break;
             } else {
-                System.out.println("    "+ input);
+                store.add(input);
+                System.out.println("    added: "+ input);
             }
         }
     }

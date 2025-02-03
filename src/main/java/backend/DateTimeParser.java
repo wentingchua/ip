f2ceba9aeefddf4920cb27ae23480feffc1c845e
wentingchua <wentingchua@gmail.com>
2025-02-04 01:37:40 +0800

    Removed unnecessary comment for readability

--- src/main/java/backend/DateTimeParser.java
+++ src/main/java/backend/DateTimeParser.java
@@ -7,7 +7,7 @@ public class DateTimeParser {
     private static final DateTimeFormatter OUTPUT_FORMAT = DateTimeFormatter.ofPattern("MMM dd yyyy, h:mma");
     public static LocalDateTime parseDateTime(String input) {
         try {
-            return LocalDateTime.parse(input); // Handles "2332-11-23T23:59"
+            return LocalDateTime.parse(input);
         } catch (DateTimeParseException e) {
             DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HHmm");
             return LocalDateTime.parse(input, formatter);

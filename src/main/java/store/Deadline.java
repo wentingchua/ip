e507ef83d5b39dda6da5cd1e923b41c5baa49709
wentingchua <wentingchua@gmail.com>
2025-02-19 12:22:25 +0800

    Enable tags to be printed on Deadline tasks

--- src/main/java/store/Deadline.java
+++ src/main/java/store/Deadline.java
@@ -29,13 +29,13 @@ public class Deadline extends Task {
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + DateTimeParser.formatDateTime(by) + ")";
+        return "[D]" + super.toString() + " (by: " + DateTimeParser.formatDateTime(by) + ")" + " " + super.tagsToString();
     }
 
     @Override
     public String toSaveFormat() {
         return "D | " + getStatusIcon() + " | " + description + " | "
-                + by.format(DateTimeFormatter.ofPattern("dd/MM/yyyy HHmm"));
+                + by.format(DateTimeFormatter.ofPattern("dd/MM/yyyy HHmm")) + " " + super.tagsToString();
     }
 
 }
